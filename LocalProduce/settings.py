0c534c1c2dbbd00837bbc3c8f1286c72945f10ad
markd-hiyield <mark@hiyield.co.uk>
2022-06-04 12:05:55 +0100

    changed settings

--- LocalProduce/settings.py
+++ LocalProduce/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 3rd-party-apps
     'rest_framework',
+    "corsheaders",
     # Local
     'accounts.apps.AccountsConfig',
     'posts.apps.PostsConfig',
@@ -53,12 +54,20 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = (
+    "http://localhost:3000",
+    "http://localhost:8000"
+)
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
 
 ROOT_URLCONF = 'LocalProduce.urls'
 
